8d052b34c759d218e55b5145c155c90e687944b5
Richard Darst <rkd@zgib.net>
2021-11-15 16:24:45 +0200

    restart.sh: Add script to restart all deployments

---
+++ restart.sh
@@ -0,0 +1,6 @@
+kubectl -n zulip-bots \
+	rollout restart \
+	deployment/zulip-bot-cs-sc-bridge \
+	deployment/zulip-bot-csit-deployment \
+	deployment/zulip-bot-nbephys-deployment \
+	deployment/zulip-bot-scicomp-deployment
