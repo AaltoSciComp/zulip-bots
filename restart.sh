a9bc905c8c3ef7bef20151eeef72de11e2ca03fd
Richard Darst <rkd@zgib.net>
2024-05-22 12:27:34 +0300

    Update jupyter.cs to jupyter.cs-public and fix restart.sh

--- restart.sh
+++ restart.sh
@@ -1,7 +1,8 @@
 kubectl -n zulip-bots \
-	rollout restart \
-	deployment/zulip-bot-cs-sc-bridge \
-	deployment/zulip-bot-cs-sc-cs-public
+        rollout restart \
+        deployment/zulip-bot-cs-sc-bridge \
+        deployment/zulip-bot-cs-sc-bridge-cs-public \
+        deployment/zulip-bot-cs-sc-bridge-jupyter
 
 #	deployment/zulip-bot-csit-deployment \
 #	deployment/zulip-bot-nbephys-deployment \
